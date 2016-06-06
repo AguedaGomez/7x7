c9716dfe9a8f7782cf414241f5b54f421509f695
Agueda Gomez Cambronero <al285895@uji.es>
2016-06-06 18:27:27 +0200

    final change to final level. If you complete the final level the game ends

--- app/src/main/java/an/an7x7/GameCore/Game7x7Model.java
+++ app/src/main/java/an/an7x7/GameCore/Game7x7Model.java
@@ -100,12 +100,6 @@ public class Game7x7Model {
             eraseLine();
             difPosBigTransparentSquare=10;
             difSideBigTransparentSquare=20;
-            /*if (newSquaresCounter == level) { // si ya ha generado los cuadrados del nivel
-                state = State.ON_GAME;
-                newSquaresCounter = 0;
-            }else{ // si no, es que ha eliminado una linea aleatoria y tienen que seguir saliendo nuevos
-                state = State.SQUARES_APPEAR;
-            }*/
             state = State.ON_GAME;
         }
         else {
@@ -114,9 +108,15 @@ public class Game7x7Model {
 
         }
         if (lines == LINES_NEXT_LEVEL){
-            lines = 0;
-            level++;
-            nextColor();
+            if (level == 6)
+                state = State.END_GAME;
+            else {
+                lines = 0;
+                level++;
+                nextColor();
+
+            }
+
         }
 
     }
